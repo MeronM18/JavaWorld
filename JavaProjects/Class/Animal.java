7729396eb922dbf726b6aee56ef3410f2d904647
MeronM18 <matti2@oakland.edu>
2024-01-30 14:23:17 -0500

    commit everything

---
+++ JavaProjects/Class/Animal.java
@@ -0,0 +1,38 @@
+public class Animal {
+    public int age;
+    public String gender; 
+
+    public void isMammal() {
+        System.out.println("This is a method from the Animal class.");
+    }
+
+    public void mate() {
+        System.out.println("This is a method from the Animal class.");
+    }
+
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Fish myFish = new Fish();
+        Zebra myZebra = new Zebra();
+
+        myAnimal.isMammal();
+        myAnimal.mate();
+        myZebra.run();
+    }
+}
+
+class Fish extends Animal {
+    private double sizeInFeet;
+
+    public void canEat() {
+        System.out.println("This is a method from the Fish class.");
+    }
+}
+
+class Zebra extends Animal {
+    public boolean is_wild;
+
+    public void run() {
+        System.out.println("This is a method from the Zebra class.");
+    }
+}
---
+++ src/src/Program.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        for(int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
+        input.close();
+    }
+}
